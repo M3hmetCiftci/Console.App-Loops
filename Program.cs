7c9fc3bea57ecd9ae4f0d08f37e9ffda02c46394
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-11-05 14:00:49 +0300

    Console.App-Loops

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //<------------------------------------if------------------->
+            //int i;
+            //Console.WriteLine("For: ");
+            ////for (i = 0; i < 10; i++)
+            //for (i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.Read();
+
+
+
+
+
+            //Console.WriteLine("for: ");
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+
+            //int i;
+            //for (i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello Word");
+            //}
+            //<------------------------------------Decision Structure------------------->
+
+
+
+            //int Number;
+            //Console.Write("Number: ");
+            //Number=Convert.ToInt32(Console.ReadLine());
+            //if (Number%2==0)
+            //{
+            //    Console.WriteLine("Double");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Only");
+            //}
+
+
+
+
+
+            //for (int i = 0; i <=20; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+            //int number;
+            //Console.Write("Number: ");
+            //number=Convert.ToInt32(Console.ReadLine()); 
+            //for (int i = 1; i <= number; i++) 
+            //{
+            //    if (number % i == 0) 
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int plus = 0;
+            //for (int i = 0; i <=20; i++)
+            //{
+            //   plus=plus + i;
+            //}
+            //Console.WriteLine(plus);
+            //Console.Read();
+
+            //<-----------------------------Factorial------------------------->
+            //int factorial = 1;
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    factorial = factorial * i;
+            //}
+            //Console.WriteLine(factorial);
+
+            //<-----------------------------While------------------------->
+
+            //int number = 1;
+            //while (number <= 3)  
+            //{
+            //    Console.WriteLine(number);
+            //    number++;
+            //}
+            //Console.Read();
+
+
+
+            //int number = 1;
+            //int plus = 0;
+            //while (number <= 10) 
+            //{
+            //    plus=plus+number;
+            //    number++;
+            //    //Console.WriteLine(plus);
+            //}
+            //Console.WriteLine(plus);
+
+
+            //<-----------------------------Do While------------------------->
+
+            //int number = 1;
+            //do 
+            //{ 
+            //number++;
+            //Console.WriteLine(number);
+            //} 
+            //while (number <= 20);
+            //Console.WriteLine(number);
+        }
+      
+    }
+}
